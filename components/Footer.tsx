877c5dbdde150e236a8d8fd429dfe75b7b2b937d
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-12-04 13:08:11 +0200

    portfolio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="w-full absolute left-0 bottom-0 min-h-96">
         <img
           // src="/footer-grid.svg"
-          src="../public/footer-grid.svg"
+          src="./footer-grid.svg"
           alt="grid"
           className="w-full h-full opacity-50"
         />
